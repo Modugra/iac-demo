d09a967a5c5cd4f39dd99c7611dec31dd9054867
Modugra <128815858+Modugra@users.noreply.github.com>
2023-05-24 10:22:09 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIARKEUAKQL7A2DRTMW"
-  secret_key = " 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"
+  access_key = "AKIA2ZCUPBOUYECCA2LK"
+  secret_key = "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"
 }
